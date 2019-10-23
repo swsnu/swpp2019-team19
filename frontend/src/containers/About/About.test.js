9a0c0b54312ff8d817b5a55c731e6ef0b4f0646e
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2019-10-23 17:44:41 +0900

    updated package dependency and test setup

---
+++ frontend/src/containers/About/About.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import About from "./About";
+import { shallow } from "enzyme";
+
+describe("<About />", () => {
+  it("renders", () => {
+    const wrapper = shallow(<About />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
