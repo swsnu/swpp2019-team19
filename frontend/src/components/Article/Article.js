f41ee20c7b792bab4436db74804d5941946899fe
ulgal <ulgal@github.com>
2019-10-26 01:37:50 +0900

    add signin

--- frontend/src/components/Article/Article.js
+++ frontend/src/components/Article/Article.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import Button from 'bootstrap';
+
 import './Article.css';
 
 const Article = (props) => (
-  <tr className="Article">
-    <th className="article_id">
-      Article_ID:
-      {props.id}
-    </th>
-    <th className="author_name">
-      Auther:
-      {props.author_name}
-    </th>
-    <th>
-      <Button className="article-title" onClick={props.clickDetail}>
+    <div className='Article'>
+      <div className='article_id'>
+        Article_ID:
+        {props.id}
+      </div>
+      <div className='author_name'>
+        Auther:
+        {props.author_name}
+      </div>
+      <button className='article-title' onClick={props.clickDetail}>
         Title:
         {props.title}
-      </Button>
-    </th>
-    <th className="tag">
-      tag:
-      {props.tag}
-    </th>
+      </button>
+      <div className='tag'>
+        tag:
+        {props.tag}
+      </div>
 
-  </tr>
-);
+    </div>
+  );
 
 export default Article;
