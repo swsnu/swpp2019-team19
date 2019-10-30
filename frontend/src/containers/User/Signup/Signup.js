7c0c492cfd0012e244f34e5126710a6861a863fa
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2019-10-30 13:31:44 +0900

    resolve signup failure

--- frontend/src/containers/User/Signup/Signup.js
+++ frontend/src/containers/User/Signup/Signup.js
@@ -10,7 +10,7 @@ class Signup extends Component {
   /* eslint-disable */
   constructor() {
     super();
-    state = {
+    this.state = {
       username: '',
       email: '',
       password: '',
