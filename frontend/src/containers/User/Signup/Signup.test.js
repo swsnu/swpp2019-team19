3a2e12b4e1af1942cfc57d1a6b4ec0e8fb2d75ed
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2019-10-31 10:58:01 +0900

    signup bug fixes and tests added (issue #10)

--- frontend/src/containers/User/Signup/Signup.test.js
+++ frontend/src/containers/User/Signup/Signup.test.js
@@ -44,4 +44,47 @@ describe('<Signup />', () => {
     const wrapper = component.find('.Signup');
     expect(wrapper.length).toBe(1);
   });
+
+  it('input text and click button, fails', () => {
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation((_str) => { });
+
+    const wrapper = mount(signup);
+    const emailInput = wrapper.find('#email-input');
+    const usernameInput = wrapper.find('#username-input');
+    const passwordInput = wrapper.find('#pw-input');
+    const passwordConfirmInput = wrapper.find('#pw-confirm-input');
+    const buttonInput = wrapper.find('#Signup-button').at(0);
+
+    emailInput.instance().value = 'a';
+    emailInput.simulate('change');
+
+    usernameInput.instance().value = 'b';
+    usernameInput.simulate('change');
+
+    passwordInput.instance().value = 'c';
+    passwordInput.simulate('change');
+
+    passwordConfirmInput.instance().value = 'd';
+    passwordConfirmInput.simulate('change');
+
+    expect(alertSpy).toHaveBeenCalledTimes(0);
+    expect(passwordInput.instance().value).toEqual('c');
+    expect(passwordConfirmInput.instance().value).toEqual('d');
+    buttonInput.simulate('click');
+    expect(passwordInput.instance().value).toEqual('');
+    expect(passwordConfirmInput.instance().value).toEqual('');
+    expect(alertSpy).toHaveBeenCalledTimes(1);
+    expect(alertSpy)
+      .toHaveBeenCalledWith('Password should be at least 8 characters');
+  });
+
+  // it('goto signin page', () => {
+  //   const wrapper = mount(signup);
+  //   const buttonInput = wrapper.find('#direct-to-signin').at(0);
+
+  //   buttonInput.simulate('click');
+
+  //   // expect();
+  // })
+  // TODO : historyMock is not applicable with this model
 });
