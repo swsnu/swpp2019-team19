c107b40dea7e3f9246b5c3940b8efcc8866fe20b
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2019-11-01 00:02:26 +0900

    comments out iterative code(issue #25)

--- frontend/src/store/reducers/User.js
+++ frontend/src/store/reducers/User.js
@@ -1,29 +1,29 @@
-import {
-  SIGN_IN,
-  SIGN_OUT,
-  SIGN_UP,
-  CHANGE_INFO,
-} from '../actions/types';
+// import {
+//   SIGN_IN,
+//   SIGN_OUT,
+//   SIGN_UP,
+//   CHANGE_INFO,
+// } from '../actions/types';
 
 const initialState = {};
 
 // TODO
 export default function (state = initialState, action) {
   switch (action.type) {
-    case SIGN_IN: {
-      return state;
-    }
-    case SIGN_OUT: {
-      return state;
-    }
+    // case SIGN_IN: {
+    //   return state;
+    // }
+    // case SIGN_OUT: {
+    //   return state;
+    // }
 
-    case SIGN_UP: {
-      return state;
-    }
+    // case SIGN_UP: {
+    //   return state;
+    // }
 
-    case CHANGE_INFO: {
-      return state;
-    }
+    // case CHANGE_INFO: {
+    //   return state;
+    // }
     default: {
       return state;
     }
