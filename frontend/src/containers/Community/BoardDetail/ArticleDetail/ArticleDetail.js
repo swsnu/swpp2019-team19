8346499780f5e0e45cb518c14ec58d6dca51d51f
desiderare <kimmando501@naver.com>
2019-11-02 15:15:39 +0900

    test version

--- frontend/src/containers/Community/BoardDetail/ArticleDetail/ArticleDetail.js
+++ frontend/src/containers/Community/BoardDetail/ArticleDetail/ArticleDetail.js
@@ -1,10 +1,11 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { Button } from 'reactstrap';
-import * as actionCreators from '../../../../store/actions';
+import * as actionCreators from '../../../../store/actions';
 
 
 class ArticleDetail extends Component {
@@ -13,22 +14,38 @@ class ArticleDetail extends Component {
   }
 
   render() {
+    const LikeHandler = () => {
+      this.props.putVote('like', this.props.storedArticle.id);
+      window.location.reload(false);
+      /* window.location.replace(`/boards/${this.props.match.params.boardName}/
+      ${this.props.match.params.articleId}/`); */
+    };
+    const DislikeHandler = () => {
+      this.props.putVote('dislike', this.props.storedArticle.id);
+      window.location.reload(false);
+    };
     return (
       <div className="ArticleDetail">
-        <Button id="direct-to-board" onClick={() => this.props.history.push(`/boards/${this.props.match.params.boardName}`)}>
-          Back to Board
+        <Button id="direct-to-board" onClick={() => this.props.history.push(`/boards/${this.props.match.params.boardName}/`)}>
+          Back to Board
         </Button>
-        Article Number:
+        <br />
+        <br />
+        Article Number:
         <p id="article-id">{this.props.storedArticle.id}</p>
         Author:
         <p id="article-author">{this.props.storedArticle.author}</p>
-        Title: 1
+        Title:
         <p id="article-title">{this.props.storedArticle.title}</p>
         Content:
         <p id="article-content">{this.props.storedArticle.content}</p>
-        Like:
+        <Button id="direct-to-like" onClick={() => LikeHandler()}>
+          like
+        </Button>
         <p id="article-like">{this.props.storedArticle.like}</p>
-        Dislike:
+        <Button id="direct-to-dislike" onClick={() => DislikeHandler()}>
+          dislike
+        </Button>
         <p id="article-dislike">{this.props.storedArticle.dislike}</p>
       </div>
     );
@@ -41,6 +58,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   fetchArticle: (id) => dispatch(actionCreators.fetchArticle(id)),
+  putVote: (vote, id) => dispatch(actionCreators.putVote(vote, id)),
 });
 
 export default connect(
