124227ca0505816ee32ad8a2b815350301866c82
ulgal <ulgal@github.com>
2019-11-05 13:40:49 +0900

    custom navbar

---
+++ frontend/src/components/Navbar/CustomNavbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Navbar, Col, Button, Modal, Nav, NavItem } from 'react-bootstrap';
+import './CustomNavbar.css';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function CustomNavbar() {
+  const [show, setShow] = useState(false);
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <React.Fragment>
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Col xs={1} md={1}>
+          <Button variant="secondary" onClick={handleShow}>
+            <FontAwesomeIcon icon={faBars} size="2x" />
+          </Button>
+        </Col>
+        <Col xs={3} md={3}>
+
+        </Col>
+        <Col xs={4} md={4}>
+          <Navbar.Brand href="http://localhost:3000/chat"><font color="yellow" size="+3">SNUBot</font></Navbar.Brand>
+        </Col>
+        <Col xs={4} md={4}>
+        </Col>
+      </Navbar >
+      <Modal show={show} onHide={handleClose} id="sidebar-wrapper">
+        <Modal.Header className="sidebar-heading">
+          <Modal.Title>Logged State</Modal.Title>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Header>
+        <Modal.Body className="sidebar-body">
+          <Nav className="flex-column">
+            <Nav.Link href="http://localhost:3000/chat">Chat</Nav.Link>
+            <Nav.Link href="http://localhost:3000/boards">Community Main</Nav.Link>
+            <Nav.Link href="http://localhost:3000/boards/all">All Board</Nav.Link>
+            <Nav.Link href="http://localhost:3000/boards/hot">Hot Board</Nav.Link>
+          </Nav>
+        </Modal.Body>
+        <Modal.Footer className="sidebar-footer">
+          Footer
+        </Modal.Footer>
+      </Modal>
+    </React.Fragment >
+  )
+}
+
+
+export default CustomNavbar;
