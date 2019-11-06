31d8792c0ca8e7e27e40e910a9e3522091352e52
ulgal <ulgal@github.com>
2019-11-06 14:55:08 +0900

    change UUID, remove submited message in chat, change img in bot's message

--- frontend/src/components/UUID/getUUID.js
+++ frontend/src/components/UUID/getUUID.js
@@ -1,7 +1,7 @@
 export default function getUUID() { // UUID v4 generator in JavaScript (RFC4122 compliant)
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = Math.random() * 16;
-    const v = ((c === 'x') ? r : ((r % 3) % 8));
+    const v = ((c === 'x') ? r : (((r % 3) * 17) % 8));
     return v.toString(16);
   });
 }
--- frontend/src/containers/Chat/Chat.js
+++ frontend/src/containers/Chat/Chat.js
@@ -24,6 +24,7 @@ class Chat extends Component {
   render() {
     const sendMessage = (message) => {
       this.props.sendMessage(message);
+      this.setState({ userInput: '' });
     };
     const chatLog = this.props.chatHistory.map((message) => {
       if (message.from === 'user') {
