af1f1f1b1007498b61fb41b9886be75aa6b9bd87
ulgal <ulgal@github.com>
2019-11-09 02:22:32 +0900

    change HOST, add makemigrations at travis

--- backend/SNUBot/SNUBot/settings.py
+++ backend/SNUBot/SNUBot/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'SNUBot',
         'USER': 'SNUBot',
         'PASSWORD': 'SNUBot',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
