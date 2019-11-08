2f7afd3c6b5c9dd3916d82dab4db9a34ec99ba45
ulgal <ulgal@github.com>
2019-11-09 02:37:03 +0900

    add travis setting

--- backend/SNUBot/SNUBot/settings.py
+++ backend/SNUBot/SNUBot/settings.py
@@ -84,9 +84,6 @@ DATABASES = {
         'PASSWORD': 'SNUBot',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-        }
     }
 }
 
