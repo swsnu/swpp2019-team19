7aace568d20789bae9176600b635904053d16482
lightb0x <lightb0x@snu.ac.kr>
2019-11-12 13:23:55 +0900

    comment out `pymysql` for local testing

--- backend/SNUBot/SNUBot/settings.py
+++ backend/SNUBot/SNUBot/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-import pymysql
+# import pymysql
 
-pymysql.install_as_MySQLdb()
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
