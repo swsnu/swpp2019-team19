3de1513314f5553844601b3ed29f8ae15a0bd098
lightb0x <lightb0x@snu.ac.kr>
2019-11-12 14:31:49 +0900

    Documentation update and mySQL settings for mac(#97) (#96)
    * python version for tensorflow and fix typo
    
    * add mysql settings
    
    * add `pymysql` to pip dependency
    
    * add `pymysql`
    
    * move to `settings.py`
    
    * move to `settings.py`
    
    * comment out `pymysql` for local testing
    
    * comment out `pymysql` for local testing
    
    * macOS-dependent mysql settings
    
    * macOS-dependent mySQL settings

--- backend/SNUBot/SNUBot/settings.py
+++ backend/SNUBot/SNUBot/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import platform
+# for macOS
+if platform.system() == 'Darwin':
+    import pymysql
+    pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
