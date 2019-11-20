c0a394b4aea7a772524b87a6b2d3d32f66d97938
ulgal <eloonsang@snu.ac.kr>
2019-11-20 23:08:49 +0900

    add korean model on admin page

--- backend/SNUBot/rasa_kor/admin.py
+++ backend/SNUBot/rasa_kor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import IntentKor, ActionKor, StoryKor, EntityKor, SlotKor
 
 # Register your models here.
+admin.site.register(IntentKor)
+admin.site.register(ActionKor)
+admin.site.register(StoryKor)
+admin.site.register(EntityKor)
+admin.site.register(SlotKor)
