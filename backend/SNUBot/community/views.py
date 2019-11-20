ea32e662cb3623d91651d3dee1cfc0eba39866b7
sungbin92 <qkr8545@gmail.com>
2019-11-20 17:13:24 +0900

    black applied, #126

--- backend/SNUBot/community/views.py
+++ backend/SNUBot/community/views.py
@@ -68,8 +68,7 @@ def signin(request):
             new_password = req_data["new_password"]
         except (KeyError, JSONDecodeError):
             return HttpResponseBadRequest()
-        user = authenticate(request, username=username,
-                            password=current_password)
+        user = authenticate(request, username=username, password=current_password)
         if user is not None:
             target_user = User.objects.get(username=username)
             target_user.set_password(new_password)
@@ -100,8 +99,7 @@ def account(request):
                 new_password = req_data["new_password"]
             except (KeyError, JSONDecodeError):
                 return HttpResponseBadRequest()
-            user = authenticate(request, username=username,
-                                password=current_password)
+            user = authenticate(request, username=username, password=current_password)
             if user is not None:
                 target_user = User.objects.get(username=username)
                 target_user.set_password(new_password)
@@ -173,14 +171,13 @@ def boards(request):
                 if article["title"].find(search_keyword) != -1
             ]
     if sort_criteria == "good":
-        article_list = sorted(
-            article_list, key=itemgetter("vote"), reverse=True)
+        article_list = sorted(article_list, key=itemgetter("vote"), reverse=True)
     elif sort_criteria == "new":
         article_list.reverse()
     max_page = math.ceil(len(article_list) / article_count)
     if len(article_list) > article_count:
         article_list = article_list[
-            article_count * (cur_page_num - 1): article_count * cur_page_num
+            article_count * (cur_page_num - 1) : article_count * cur_page_num
         ]
     return_list = [max_page, article_list]
     return JsonResponse(return_list, safe=False)
