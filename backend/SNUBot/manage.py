52ef9dd214ab128dd3086e1825be1172fddc0ba6
ulgal <eloonsang@snu.ac.kr>
2019-11-21 13:53:44 +0900

    basic multiprocessing

--- backend/SNUBot/manage.py
+++ backend/SNUBot/manage.py
@@ -2,10 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import multiprocessing as mp
+import time
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SNUBot.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SNUBot.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +19,16 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+def foo():
+    while True:
+        print("hi")
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    mp.set_start_method("spawn")
+    q = mp.Queue()
+    p1 = mp.Process(target=main)
+    p2 = mp.Process(target=foo)
+    p1.start()
+    p2.start()
