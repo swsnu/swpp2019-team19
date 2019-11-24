a4ceb94b0295966be215d9f3d70d5f0bd6b13de9
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2019-11-24 23:22:39 +0900

    eslint disable change

--- frontend/src/containers/Chat/Chat.js
+++ frontend/src/containers/Chat/Chat.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,5 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { configure } from 'enzyme';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
