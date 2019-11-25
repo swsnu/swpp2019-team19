32e06b72f7aeda5cd18d3968757aab635a7fd81f
Ubuntu <swppTEAM19@SNUBot.imp0xsp33zpeppa4qd1t5spypa.syx.internal.cloudapp.net>
2019-11-25 16:34:43 +0000

    add if state to remove get request

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 // eslint-disable-next-line import/no-named-as-default
 import axios from 'axios';
+import Cookie from 'js-cookie';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store, { history } from './store/store';
@@ -12,7 +13,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.withCredentials = true;
-axios.get('/api/token/');
+if(Cookie.get().csrftoken===undefined){
+	axios.get('/api/token/');
+}
 ReactDOM.render(
   <Provider store={store}>
     <App history={history} />
