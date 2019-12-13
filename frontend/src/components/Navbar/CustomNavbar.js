7538d8d26c00e947244f79281f5aaa703579e599
Myeonghwan Ahn <lightb0x@snu.ac.kr>
2019-12-13 23:25:37 +0900

    header fine tune

--- frontend/src/components/Navbar/CustomNavbar.js
+++ frontend/src/components/Navbar/CustomNavbar.js
@@ -102,7 +102,7 @@ function CustomNavbar(props) {
         </div>
       ) : (null)}
       <Navbar className="custom-navbar" bg="dark" expand="lg">
-        <Col xs={1} sm={1} md={1}>
+        <Col>
           <Button
             className="sidebar-show-button float-left"
             variant="link"
@@ -111,8 +111,7 @@ function CustomNavbar(props) {
             <FontAwesomeIcon icon={faBars} size="2x" />
           </Button>
         </Col>
-        <Col xs={0} sm={2} md={3} />
-        <Col xs={6} sm={2} md={4}>
+        <Col xs={4} sm={6} md={8} lg={10} xl={10}>
           <Navbar.Brand
             onClick={() => redirectAndClose('/chat')}
             style={{
@@ -125,7 +124,7 @@ function CustomNavbar(props) {
             </font>
           </Navbar.Brand>
         </Col>
-        <Col xs={1} sm={3} md={4} />
+        <Col />
       </Navbar>
     </>
   );
