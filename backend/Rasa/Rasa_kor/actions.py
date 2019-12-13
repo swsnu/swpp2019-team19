8269ac4878235648b01b713482ca1901ab63989c
Seongmin Park <48579185+ulgal@users.noreply.github.com>
2019-12-13 12:04:10 +0900

    Rasa action redis (#193)
    * loadtest file
    
    * add redis at rasa action server, cache-remove script

--- backend/Rasa/Rasa_kor/actions.py
+++ backend/Rasa/Rasa_kor/actions.py
@@ -12,7 +12,9 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 import requests
+import redis
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
 time = ["아침", "점심", "저녁"]
 engineering = ["교수식당", "푸드코트"]
@@ -33,81 +35,89 @@ class ActionMeal(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
         meal = tracker.get_slot("meal")
-        response = requests.get("http://mini.snu.ac.kr/cafe/today")
-        if response.status_code != 200:
-            dispatcher.ustter_message("mini.snu.ac.kr doesn't reply")
+        cached = redis.StrictRedis(host="127.0.0.1", port=6379, db=2)
+        dt = datetime.now() + timedelta(hours=9)
+        tg_str = str(dt.month) + str(dt.day) + meal + "kor"
+        tg = cached.get(tg_str)
+        if not tg:
+            response = requests.get("http://mini.snu.ac.kr/cafe/today")
+            if response.status_code != 200:
+                dispatcher.ustter_message("mini.snu.ac.kr doesn't reply")
+                return [SlotSet("meal", meal)]
+
+            parsed_soup = BeautifulSoup(response.content, "html.parser")
+
+            trs = parsed_soup.find_all("tr")
+            targets = []
+            for tr in trs:
+                for td in tr.td.contents:
+                    if meal in td:
+                        targets.append(tr)
+
+            for target in targets:
+                [s.extract() for s in target("span")]
+
+            k = 0
+            response_message = ""
+            try:
+                if targets == []:
+                    response_message = "정확한 식당 이름을 알려주세요."
+                elif meal == "301":
+                    for target in targets:
+                        response_message = (
+                            response_message + engineering[k] + "<br>"
+                        )
+                        k = k + 1
+                        for child in target.contents[2].children:
+                            response_message = (
+                                response_message + str(child) + "<br>"
+                            )
+                elif meal == "공깡":
+                    for target in targets:
+                        response_message = (
+                            response_message + eng_ggang[k] + "<br>"
+                        )
+                        k = k + 1
+                        for child in target.contents[2].children:
+                            response_message = (
+                                response_message + str(child) + "<br>"
+                            )
+                elif meal == "두레미담":
+                    for target in targets:
+                        response_message = (
+                            response_message + agricultural[k] + "<br>"
+                        )
+                        k = k + 1
+                        for child in target.contents[2].children:
+                            response_message = (
+                                response_message + str(child) + "<br>"
+                            )
+                elif meal == "기숙사":
+                    for target in targets:
+                        response_message = (
+                            response_message + dormitory[k] + "<br>"
+                        )
+                        k = k + 1
+                        for child in target.contents[2].children:
+                            response_message = (
+                                response_message + str(child) + "<br>"
+                            )
+                else:
+                    if not meal == "학생회관":
+                        k = 1
+                    for target in targets:
+                        response_message = response_message + time[k] + "<br>"
+                        k = k + 1
+                        for child in target.contents[2].children:
+                            response_message = (
+                                response_message + str(child) + "<br>"
+                            )
+            except IndexError as e:
+                dispatcher.utter_message(idxerr_msg)
+                return [SlotSet("meal", meal)]
+
+            dispatcher.utter_message(response_message)
+            cached.set(tg_str, response_message)
             return [SlotSet("meal", meal)]
-
-        parsed_soup = BeautifulSoup(response.content, "html.parser")
-
-        trs = parsed_soup.find_all("tr")
-        targets = []
-        for tr in trs:
-            for td in tr.td.contents:
-                if meal in td:
-                    targets.append(tr)
-
-        for target in targets:
-            [s.extract() for s in target("span")]
-
-        k = 0
-        response_message = ""
-        try:
-            if targets == []:
-                response_message = "정확한 식당 이름을 알려주세요."
-            elif meal == "301":
-                for target in targets:
-                    response_message = (
-                        response_message + engineering[k] + "<br>"
-                    )
-                    k = k + 1
-                    for child in target.contents[2].children:
-                        # print(child)
-                        response_message = (
-                            response_message + str(child) + "<br>"
-                        )
-            elif meal == "공깡":
-                for target in targets:
-                    response_message = response_message + eng_ggang[k] + "<br>"
-                    k = k + 1
-                    for child in target.contents[2].children:
-                        # print(child)
-                        response_message = (
-                            response_message + str(child) + "<br>"
-                        )
-            elif meal == "두레미담":
-                for target in targets:
-                    response_message = (
-                        response_message + agricultural[k] + "<br>"
-                    )
-                    k = k + 1
-                    for child in target.contents[2].children:
-                        # print(child)
-                        response_message = (
-                            response_message + str(child) + "<br>"
-                        )
-            elif meal == "기숙사":
-                for target in targets:
-                    response_message = response_message + dormitory[k] + "<br>"
-                    k = k + 1
-                    for child in target.contents[2].children:
-                        # print(child)
-                        response_message = (
-                            response_message + str(child) + "<br>"
-                        )
-            else:
-                if not meal == "학생회관":
-                    k = 1
-                for target in targets:
-                    response_message = response_message + time[k] + "<br>"
-                    k = k + 1
-                    for child in target.contents[2].children:
-                        response_message = (
-                            response_message + str(child) + "<br>"
-                        )
-        except IndexError as e:
-            dispatcher.utter_message(idxerr_msg)
-            return [SlotSet("meal", meal)]
-
-        dispatcher.utter_message(response_message)
+        dispatcher.utter_message(tg)
         return [SlotSet("meal", meal)]
