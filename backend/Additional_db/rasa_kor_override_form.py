b3730a64789c1e256091cb96f52f633d301c10a9
sungbin92 <qkr8545@gmail.com>
2019-12-14 12:42:11 +0900

    fix override kor answer

--- backend/Additional_db/rasa_kor_override_form.py
+++ backend/Additional_db/rasa_kor_override_form.py
@@ -7,7 +7,7 @@ IntentKor(
 action = ActionKor(
     action_name="utter_give_override_file",
     action_type="text",
-    text_value="<a href='https://cse.snu.ac.kr/sites/default/files/node--notice/2018%EB%85%84%201%ED%95%99%EA%B8%B0%20%EC%88%98%EA%B0%95%EC%8B%A0%EC%B2%AD%20%EC%A0%95%EC%A0%95%20%EC%9A%94%EC%B2%AD%EC%84%9C%28%EC%B4%88%EC%95%88%EC%A7%80%29.hwp'>여기</a> 누르면 다운로드 되요",
+    text_value="<a href='https://cse.snu.ac.kr/sites/default/files/node--notice/2018%EB%85%84%201%ED%95%99%EA%B8%B0%20%EC%88%98%EA%B0%95%EC%8B%A0%EC%B2%AD%20%EC%A0%95%EC%A0%95%20%EC%9A%94%EC%B2%AD%EC%84%9C%28%EC%B4%88%EC%95%88%EC%A7%80%29.hwp'>여기</a>",
 )
 action.save()
 action.intent.add(IntentKor.objects.get(intent_name="request_override_file"))
