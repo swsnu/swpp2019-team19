450eb79125bdf479d5a051a5fa437de326352253
sungbin92 <qkr8545@gmail.com>
2019-12-14 10:46:16 +0900

    fix action text value

--- backend/Additional_db/rasa_eng_override_form.py
+++ backend/Additional_db/rasa_eng_override_form.py
@@ -1,20 +1,20 @@
-from rasa_eng.models import  IntentEng, ActionEng, StoryEng
+from rasa_eng.models import IntentEng, ActionEng, StoryEng
 
 IntentEng(
-  intent_name="request_override_file",
-  intent_tokens=[
-    "give me override file",
-    "give me cho an gee",
-    "how can I get override file",
-    "how can I get cho an gee",
-    "give me override form",
-    "how can I get override form",
-  ]
+    intent_name="request_override_file",
+    intent_tokens=[
+        "give me override file",
+        "give me cho an gee",
+        "how can I get override file",
+        "how can I get cho an gee",
+        "give me override form",
+        "how can I get override form",
+    ],
 ).save()
 action = ActionEng(
-  action_name="utter_give_override_file",
-  action_type="text",
-  text_value="Click <a href='https://cse.snu.ac.kr/sites/default/files/node--notice/Over-ride%20Form_4.hwp'>here</a> to open the override form",
+    action_name="utter_give_override_file",
+    action_type="text",
+    text_value="Click <a href='https://cse.snu.ac.kr/sites/default/files/node--notice/Over-ride%20Form_4.hwp'>here</a> to open the override form",
 )
 action.save()
 action.intent.add(IntentEng.objects.get(intent_name="request_override_file"))
