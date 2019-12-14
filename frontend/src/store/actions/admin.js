03ba47f2ae0a8289c65561625da8db4a9fada696
Seongmin Park <48579185+ulgal@users.noreply.github.com>
2019-12-15 06:24:53 +0900

    add makefile (#233)

--- frontend/src/store/actions/admin.js
+++ frontend/src/store/actions/admin.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
 
-export const updateServer = () => axios.get('/admin_command/update/');
+export const updateServer = () => {
+  axios.get('/rasa_kor/makefile/');
+  axios.get('/rasa_eng/makefile/').then(
+    axios.get('/admin_command/update/')
+  )
+}
 export const updateModel = () => axios.get('/admin_command/replace/');
