1b6c95219e2482dd2c1c1447501a5c40cd56ce86
ulgal <eloonsang@snu.ac.kr>
2019-12-15 05:42:50 +0900

    add db migrate

--- backend/SNUBot/rerun_server.sh
+++ backend/SNUBot/rerun_server.sh
@@ -4,14 +4,16 @@ fuser -k 5005/tcp
 fuser -k 5010/tcp
 fuser -k 5055/tcp
 fuser -k 5060/tcp
+tmux send-keys -t django:0.0 C-z 'python manage.py makemigrations' Enter
 tmux send-keys -t rasa_eng:0.0 C-z 'rm models/*' Enter
 tmux send-keys -t rasa_eng:0.0 C-z 'rasa train' Enter
 tmux send-keys -t rasa_kor:0.0 C-z 'rm models/*' Enter
 tmux send-keys -t rasa_kor:0.0 C-z 'rasa train' Enter
+sleep 10
+tmux send-keys -t django:0.0 C-z 'python manage.py migrate' Enter
+
+sleep 30
 tmux send-keys -t django:0.0 C-z 'uwsgi --socket :8001 --module SNUBot.wsgi' Enter
-
-sleep 40
-
 tmux send-keys -t rasa_eng:0.0 C-z 'python -m rasa run -m ./models --endpoints ./endpoints.yml --port 5010 --log-file ./logs/rasa.log -vv --enable-api --cors "*"' Enter
 tmux send-keys -t rasa_eng_action:0.0 C-z 'rasa run actions' Enter
 tmux send-keys -t rasa_kor:0.0 C-z 'python -m rasa run -m ./models --endpoints ./endpoints.yml --port 5005 --log-file ./logs/rasa.log -vv --enable-api --cors "*"' Enter
